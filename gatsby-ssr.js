a7d0474e143e85b823d532bb2239e75db8877dcb
Mick Schroeder <mschroeder@gmail.com>
2020-12-02 06:27:56 -0500

    move css

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -7,8 +7,6 @@
 // You can delete this file if you're not using it
 exports.onRenderBody = ({ setHtmlAttributes }) => {
   setHtmlAttributes({
-    lang: "en",
-    className:
-      "h-screen leading-normal text-lg bg-gray-100 dark:bg-gray-900 font-body text-gray-900 dark:text-gray-50 antialiased",
+    lang: "en"
   })
 }
