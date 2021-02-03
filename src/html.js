27daf528e701b6464753c6bf844350f929e2d23d
Mick Schroeder <mschroeder@gmail.com>
2021-02-03 07:55:00 -0500

    update script

--- src/html.js
+++ src/html.js
@@ -23,7 +23,6 @@ export default function HTML(props) {
         {props.postBodyComponents}
       </body>
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-      <script dangerouslySetInnerHTML={{__html: '(window.adsbygoogle = window.adsbygoogle || []).push({});'}}></script>
     </html>
   )
 }
