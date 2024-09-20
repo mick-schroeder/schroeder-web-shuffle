bdf6df6ad27af28c3129a52eccbb01af6e3b27bf
Mick Schroeder <mschroeder@gmail.com>
2024-09-20 17:23:43 -0400

    💩

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,7 +4,7 @@ import { Link, HeadFC, PageProps } from "gatsby";
 const pageStyles = {
   color: "#232129",
   padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+  fontFamily: "-apple-system, sans-serif, serif",
 };
 const headingStyles = {
   marginTop: 0,
